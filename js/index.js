e88b2bbacaf7877173ccc3006bc9beacd3c34a83
CHU1226 <61220661+CHU1226@users.noreply.github.com>
2021-12-19 09:56:25 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,32 +2,41 @@ var start1 = function(){
     let i = 0;
     let time = setInterval(() => {
         i += 1;
-        randomFood();
+        f1.newFood();
         if (i == 10) {
             clearInterval(time);
         }
     }, 100);
-}
-function randomFood() {
-    let schoolFood = ['LOUISA', '小木屋鬆餅', '麥味登', '誠字號', '全家', '自助餐', '素還真', '四海遊龍', '橙廚義麵', '蔥抓餅'];
-    let num = Math.floor(Math.random(schoolFood.length + 1) * 10);
-    let answer = document.querySelector('.food-block p');
-    answer.textContent = schoolFood[num]; 
 }
 var start2 = function(){
     let i = 0;
     let time = setInterval(() => {
         i += 1;
-        randomFood2();
+        f2.newFood();
         if (i == 10) {
             clearInterval(time);
         }
     }, 100);
 }
-function randomFood2() {
-    let streetFood = ['元味便當', '光華垃圾麵', '陳記燒臘', '光華飯包', '小四川', '來來炒飯', '銀記', '麵足飯飽', '嵐迪', '咖食堂']
-    let num = Math.floor(Math.random(streetFood.length + 1) * 10);
-    let answer = document.querySelector('.food-block p');
-    answer.textContent = streetFood[num]
+
+function randomFood(food) {
+    this.food = food
 }
+
+function randomFood2(food){
+    randomFood.call(this,food);
+}
+
+randomFood2.prototype = Object.create(randomFood.prototype);
+randomFood2.prototype.constructor = randomFood2
+randomFood2.prototype.newFood = function(){
+    let num = Math.floor(Math.random(this.food.length + 1) * 10);
+    let answer = document.querySelector('.food-block p');
+    answer.textContent = this.food[num];
+}
+
+var f1 = new randomFood2(['LOUISA', '小木屋鬆餅', '麥味登', '誠字號', '全家', '自助餐', '素還真', '四海遊龍', '橙廚義麵', '蔥抓餅']);
+var f2 = new randomFood2(['元味便當', '光華垃圾麵', '陳記燒臘', '光華飯包', '小四川', '來來炒飯', '銀記', '麵足飯飽', '嵐迪', '咖食堂'])
+
+
 
